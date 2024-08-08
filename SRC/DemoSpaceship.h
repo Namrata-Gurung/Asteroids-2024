ce0e0990f1a2d58ff5843b7c048bd777c78db19a
Namrata-Gurung <125033360+Namrata-Gurung@users.noreply.github.com>
2024-08-08 20:54:00 +0100

    Created DemoSpaceship.cpp/.h

---
+++ SRC/DemoSpaceship.h
@@ -0,0 +1,39 @@
+/*#ifndef __DEMO_SPACESHIP_H__
+#define __DEMO_SPACESHIP_H__
+
+#include "GameUtil.h"
+#include "GameObject.h"
+#include "Shape.h"
+
+class DemoSpaceship : public GameObject
+{
+public:
+	DemoSpaceship();
+	DemoSpaceship(GLVector3f p, GLVector3f v, GLVector3f a, GLfloat h, GLfloat r);
+	DemoSpaceship(const DemoSpaceship& ds);
+	virtual ~DemoSpaceship(void);
+
+	virtual void Update(int t);
+	virtual void Render(void);
+
+	virtual void Thrust(float t);
+	virtual void Rotate(float r);
+	virtual void Shoot(void);
+
+	void SetDemoSpaceshipShape(shared_ptr<Shape> demo_spaceship_shape) { mDemoSpaceshipShape = demo_spaceship_shape; }
+	void SetThrusterShape(shared_ptr<Shape> thruster_shape) { mThrusterShape = thruster_shape; }
+	void SetBulletShape(shared_ptr<Shape> bullet_shape) { mBulletShape = bullet_shape; }
+
+	bool CollisionTest(shared_ptr<GameObject> o);
+	void OnCollision(const GameObjectList& objects);
+
+private:
+	float mThrust;
+
+	shared_ptr<Shape> mSpaceshipShape;
+	shared_ptr<Shape> mThrusterShape;
+	shared_ptr<Shape> mBulletShape;
+};
+
+#endif
+*/
