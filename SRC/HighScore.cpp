15e7acfe8c5ab9eff3707f6d6e30e4e64340d3cd
Namrata-Gurung <125033360+Namrata-Gurung@users.noreply.github.com>
2024-08-09 16:07:03 +0100

    Updated HighScore.cpp and HighScore.h

--- SRC/HighScore.cpp
+++ SRC/HighScore.cpp
@@ -1,84 +1,57 @@
-#include "BoundingSphere.h"
-#include "GUILabel.h"
-#include "Explosion.h"
 #include "HighScore.h"
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include "ScoreKeeper.h"
 
-// PUBLIC INSTANCE CONSTRUCTORS ///////////////////////////////////////////////
+using namespace std;
 
-/** Constructor. Takes arguments from command line, just in case. */
-Asteroids::Asteroids(int argc, char* argv[])
-	: GameSession(argc, argv)
-	Asteroids::Asteroids(int argc, char* argv[])
-	: GameSession(argc, argv), mHighScore("scores.txt")
-{
-	mLevel = 0;
-	mAsteroidCount = 0;
-
-
-	@@ - 279, 6 + 280, 20 @@ void Asteroids::CreateGUI()
-		shared_ptr<GUIComponent> start_msg_component
-		= static_pointer_cast<GUIComponent>(mStartMsgLabel);
-	mGameDisplay->GetContainer()->AddComponent(start_msg_component, GLVector2f(0.5f, 0.4f));
-
-	// high score label
-	// Create a new GUILabel and wrap it up in a shared_ptr
-	mHighScoreLabel = shared_ptr<GUILabel>(new GUILabel(""));
-	// Set the horizontal alignment of the label to GUI_HALIGN_CENTER
-	mHighScoreLabel->SetHorizontalAlignment(GUIComponent::GUI_HALIGN_CENTER);
-	// Set the vertical alignment of the label to GUI_VALIGN_MIDDLE
-	mHighScoreLabel->SetVerticalAlignment(GUIComponent::GUI_VALIGN_MIDDLE);
-	// Set the visibility of the label to false (hidden)
-	mHighScoreLabel->SetVisible(false);
-	// Add the GUILabel to the GUIContainer  
-	shared_ptr<GUIComponent> high_score_component
-		= static_pointer_cast<GUIComponent>(mHighScoreLabel);
-	mGameDisplay->GetContainer()->AddComponent(high_score_component, GLVector2f(0.5f, 0.5f));
+HighScore::HighScore(const string& filename)
+    : mScoresFile("scores.txt") {
+    LoadHighScore();
 }
 
-void Asteroids::OnScoreChanged(int score)
-
-@@ - 289, 6 + 304, 9 @@ void Asteroids::OnScoreChanged(int score)
-// Get the score message as a string
-std::string score_msg = msg_stream.str();
-mScoreLabel->SetText(score_msg);
-
-//update the scores text file
-mHighScore.AddHighScore(score);
-}
-
-void Asteroids::OnPlayerKilled(int lives_left)
-
-@@ - 312, 6 + 330, 9 @@ void Asteroids::OnPlayerKilled(int lives_left)
-	else
-	{
-		SetTimer(500, SHOW_GAME_OVER);
-		//the high score methods 
-		updateHighScore();
-		displayHighScore();
-	}
+HighScore::~HighScore() {
+    SaveHighScore();
 }
 
 
-@@ - 327, 4 + 348, 21 @@ shared_ptr<GameObject> Asteroids::CreateExplosion()
-return explosion;
+
+void HighScore::AddHighScore(int score) {
+    mScore.push_back(score);
+    SortHighScores();
 }
 
-// score table methods 
-// score table methods 
-
-void Asteroids::updateHighScore() {
-	int currentScore = mScoreKeeper.GetScore();
-	mHighScore.AddHighScore(currentScore);
-	mHighScore.SaveHighScore();
+// saving the scores in the file scores.txt
+void HighScore::SaveHighScore() {
+    ofstream file(mScoresFile);
+    if (file.is_open()) {
+        for (int score : mScore ) {
+            file << score << endl;
+        }
+        file.close();
+    }
 }
 
-void Asteroids::displayHighScore() {
-	const auto& scores = mHighScore.ListHighScores();
-	std::ostringstream hs_stream;
-	hs_stream << "High Scores: \n";
-	for (auto score : scores) {
-		hs_stream << score << "\n";
-	}
-	mHighScoreLabel->SetText(hs_stream.str());
-	mHighScoreLabel->SetVisible(true);
+//Loading scores in the file 
+void HighScore::LoadHighScore() {
+    ifstream file(mScoresFile);
+    if (file.is_open()) {
+        int score;
+        while (file >> score) {
+            mScore.push_back(score);
+        }
+        file.close();
+        SortHighScores();
+    }
+}
+
+//sorting the scores
+void HighScore::SortHighScores() {
+    sort(mScore.begin(), mScore.end(), greater<int>());
+}
+
+// Return the scores 
+const std::vector<int>& HighScore::ListHighScores() const {
+    return mScore;
 }
--- SRC/HighScore.h
+++ SRC/HighScore.h
@@ -1,1 +1,45 @@
-#pragma once
+#ifndef _HIGHSCORE_H_
+#define _HIGHSCORE_H_
+
+#include <vector>
+#include <string>
+
+class HighScore
+{
+
+public:
+
+	HighScore(const std::string& highScores = "scores.txt");
+	virtual ~HighScore(void);
+
+
+	// Save high scores to a text file 
+	void SaveHighScore();
+
+	// Add a new high score 
+	void AddHighScore(int score);
+
+	// Load high scores from a text file 
+	void LoadHighScore();
+
+	// List high scores from a vector 
+	const std::vector<int>& ListHighScores() const;
+
+private:
+
+	//File
+	std::string mScoresFile;
+
+	//Vector to store high scores
+	std::vector<int> mScore;
+
+	//sort method declaration
+	void SortHighScores();
+
+	// Set a max number of high scores to keep
+	const static size_t MAX_HIGH_SCORES = 5;
+
+};
+
+
+#endif
